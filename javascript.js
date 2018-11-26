95385e8a5932bcdf353f159a40487103aef9a717
romainbon <romaiinn1302@gmail.com>
2018-11-26 10:07:38 +0100

    Continuer le Resultat2

--- javascript.js
+++ javascript.js
@@ -1,17 +1,62 @@
+var TabRecUn = [];
+var TabRecDeux = [];
+
+var TabDepUn = [];
+var TabDepDeux = [];
+
+var TabEpargne = [];
 
 
-document.getElementById('SubmitDepense').addEventListener("click",
+// Tableau des Depense
+
+document.getElementById('SubmitDepense2').addEventListener("click",
 
     function () {
 
         var DepenseUn = document.getElementById('DepenseUn').value;
         var DepenseDeux = document.getElementById('DepenseDeux').value;
 
-        document.getElementById('Resultat').innerHTML += DepenseUn + ' : ' + DepenseDeux + '<br>';
+        document.getElementById('Resultat').innerHTML += DepenseUn + ' : ' + DepenseDeux + '<br>' ;
+        TabRecUn.push(DepenseDeux);
 
+        Total();
 });
 
-document.getElementById('SubmitRecette').addEventListener("click",
+document.getElementById('SubmitDepense1').addEventListener("click",
+
+    function () {
+
+        var DepenseTrois = document.getElementById("Nature-Select").options[document.getElementById('Nature-Select').selectedIndex].text;
+        var DepenseQuatre = document.getElementById('DepenseQuatre').value;
+
+        document.getElementById('Resultat').innerHTML += DepenseTrois + ' : ' + DepenseQuatre + '<br>' ;
+        TabRecDeux.push(DepenseQuatre);
+
+        Total();
+    });
+
+        function Total() {
+
+
+        var totalD2 = 0;
+        for (var i = 0; i < TabRecUn.length; i++) {
+            totalD2 += parseFloat(TabRecUn[i]);
+
+        }
+
+        var totalD = 0;
+        for (var i = 0; i < TabRecDeux.length; i++) {
+            totalD += parseFloat(TabRecDeux[i]);
+        }
+
+        document.getElementById('TitreResultat').innerHTML = totalD + totalD2 + " euros "; }
+
+
+
+
+// Tableau des Recettes
+
+document.getElementById('SubmitRecette2').addEventListener("click",
 
     function () {
 
@@ -19,8 +64,41 @@ document.getElementById('SubmitRecette').addEventListener("click",
         var RecetteDeux = document.getElementById('RecetteDeux').value;
 
         document.getElementById('Resultat2').innerHTML += RecetteUn + ' : ' + RecetteDeux + '<br>';
+        TabDepUn.push(RecetteDeux);
 
+        Total2();
     });
+
+document.getElementById('SubmitRecette1').addEventListener("click",
+
+    function () {
+
+        var RecetteTrois = document.getElementById('NatureSelect').value;
+        var RecetteQuatre = document.getElementById('RecetteQuatre').value;
+
+        document.getElementById('Resultat2').innerHTML += RecetteTrois + ' : ' + RecetteQuatre + '<br>';
+        TabDepDeux.push(RecetteQuatre);
+
+        Total2();
+    });
+
+function Total2() {
+
+
+    var totalR2 = 0;
+    for (var i = 0; i < TabDepUn.length; i++) {
+        totalR2 += parseFloat(TabDepUn[i]);
+
+    }
+
+    var totalR = 0;
+    for (var i = 0; i < TabDepDeux.length; i++) {
+        totalR += parseFloat(TabDepDeux[i]);
+    }
+
+    document.getElementById('TitreResultat2').innerHTML = totalR + totalR2 + " euros "; }
+
+// Tableau Epargne
 
 document.getElementById('SubmitEpargne').addEventListener("click",
 
@@ -30,10 +108,19 @@ document.getElementById('SubmitEpargne').addEventListener("click",
         var EpargneDeux = document.getElementById('EpargneDeux').value;
 
         document.getElementById('Resultat3').innerHTML += EpargneUn + ' : ' + EpargneDeux + '<br>';
+        TabEpargne.push(EpargneDeux);
 
-        document.getElementById('TitreResultat').innerHTML += EpargneUn + EpargneUn ;
+        TotalE();
     });
 
 
-/*var Depense1 = document.getElementById('DepenseUn').value;
-document.getElementById('TitreResultat').innerHTML += Depense1;*/
+function TotalE() {
+
+
+    var totalE = 0;
+    for (var i = 0; i < TabEpargne.length; i++) {
+        totalE += parseFloat(TabEpargne[i]);
+
+    }
+
+    document.getElementById('titreResultat3').innerHTML = totalE + " euros "; }
